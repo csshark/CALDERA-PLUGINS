f9e98e72005ee943f730e58483b28a894a9b2691
Robert Strzoda <roberts.wp.pl@wp.pl>
2026-01-12 17:59:02 +0100

    Update hook.py

--- dlp/hook.py
+++ dlp/hook.py
@@ -4,8 +4,19 @@ class DlpPlugin(BasePlugin):
     def __init__(self, services):
         super().__init__(services)
         self.name = 'DLP'
-        self.description = 'Data Loss Prevention testing'
+        self.description = 'Data Loss Prevention testing plugin'
         
     async def enable(self):
-        self.log.info('DLP plugin loading')
-        await self.services.get('data_svc').load_data(self._get_data_dir(), 'abilities')
+        self.log.info('DLP plugin loading...')
+        
+        await self.services.get('data_svc').load_data(
+            self._get_data_dir() / 'abilities', 
+            'abilities'
+        )
+        
+        await self.services.get('data_svc').load_data(
+            self._get_data_dir() / 'adversaries',
+            'adversaries'
+        )
+        
+        self.log.info('DLP plugin loaded successfully')
